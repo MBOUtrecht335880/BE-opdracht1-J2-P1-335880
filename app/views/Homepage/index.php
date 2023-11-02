0adc048ec11930982c6558da792127cf3b91ec41
Pershiya Sharifi <335880@mboutrecht.nl>
2023-11-02 15:22:20 +0100

    sixth commit

--- app/views/Homepage/index.php
+++ app/views/Homepage/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,8 +8,10 @@
     <link rel="stylesheet" href="../../../public/css/style.css?v=<?php echo time(); ?>">
     <title>Homepage</title>
 </head>
+
 <body>
     <h1><?= $data['title']; ?></h1>
-    <a href="<?= URLROOT; ?>/Instructeur/overzichtinstructeur">Instructeurs in dienst</a> 
+    <a href="<?= URLROOT; ?>/Instructeur/overzichtinstructeur">Instructeurs in dienst</a>
 </body>
+
 </html>
